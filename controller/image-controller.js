4c27ce722883b4160f350622ab68c529b12db885
balrajegorad <balugorad3345@gmail.com>
2024-04-20 12:50:33 +0530

    Screenshot

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,5 +1,4 @@
 import File from '../models/file.js';
-import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,7 +11,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `http://localhost:${process.env.PORT}/file/${file._id}`});
+        response.status(200).json({ path: `https://file-share-server-3.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
